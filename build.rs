56acb2dc76fbf0359598d1170754a3897858e954
Daniele Conti <fourlastor@users.noreply.github.com>
2023-11-24 14:36:39 +0100

    Conditionally apply linker arguments

--- build.rs
+++ build.rs
@@ -1,6 +1,20 @@
+use std::env;
+
 fn main() {
     #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-arg=/EXPORT:NvOptimusEnablement");
-    #[cfg(target_os = "windows")]
-    println!("cargo:rustc-link-arg=/EXPORT:AmdPowerXpressRequestHighPerformance");
+    enable_gpu_flags();
+}
+
+fn enable_gpu_flags() {
+    if requires_gpu_flag() {
+        println!("cargo:rustc-link-arg=/EXPORT:NvOptimusEnablement");
+        println!("cargo:rustc-link-arg=/EXPORT:AmdPowerXpressRequestHighPerformance");
+    }
+}
+
+fn requires_gpu_flag() -> bool {
+    match env::var("CARGO_FEATURE_WIN_NO_GPU") {
+        Ok(_) => false,
+        _ => true,
+    }
 }
